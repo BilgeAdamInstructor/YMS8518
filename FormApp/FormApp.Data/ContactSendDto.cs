c7e2d919f5e42ea63b0e211062615dfb56469e6d
Erçin Dedeoğlu <e.dedeoglu@gmail.com>
2019-11-30 19:28:55 +0300

    30.11.2019

---
+++ FormApp/FormApp.Data/ContactSendDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FormApp.Data
+{
+    public class ContactSendDto
+    {
+        [Required, MinLength(2), MaxLength(150)]
+        public string FullName { get; set; }
+
+        [Required, MinLength(6), MaxLength(345)]
+        public string Email { get; set; }
+
+        [Required, MinLength(2), MaxLength(1024)]
+        public string Message { get; set; }
+    }
+}
---
+++ FormApp/FormApp/Controllers/ContactController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FormApp.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Send([FromBody]FormApp.Data.ContactSendDto contactSendDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Hayırdır?");
+            }
+            return new JsonResult(contactSendDto);
+        }
+    }
+}
